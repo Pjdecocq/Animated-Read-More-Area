bc3c31dcf3e39c11c4b836c6334d4bc46bcf647a
Paul <pjdecocq@gmail.com>
2017-02-07 11:56:56 +0100

    Update animated-rm.js

--- animated-rm.js
+++ animated-rm.js
@@ -1,37 +1,37 @@
 jQuery(document).ready(function($){
   
-  /* Global click toggle function used by other functions */
-  $.fn.clickToggle = function(func1, func2) {
-      var funcs = [func1, func2];
-      this.data('toggleclicked', 0);
-      this.click(function() {
-          var data = $(this).data();
-          var tc = data.toggleclicked;
-          $.proxy(funcs[tc], this)();
-          data.toggleclicked = (tc + 1) % 2;
-      });
-      return this;
-  };
+	/* Global click toggle function used by other functions */
+	$.fn.clickToggle = function(func1, func2) {
+	    var funcs = [func1, func2];
+	    this.data('toggleclicked', 0);
+	    this.click(function() {
+		var data = $(this).data();
+		var tc = data.toggleclicked;
+		$.proxy(funcs[tc], this)();
+		data.toggleclicked = (tc + 1) % 2;
+	    });
+	    return this;
+	};
   
-  /*
-		The default minified height of a read more for example is: 70px
-		To animate to it's full height we need to know and set the full height at forehand
-		so we can tell the animate function to which height the span will be in it's full extended height.
-	*/
+	/*
+	 * The default minified height of a read more for example is: 70px
+	 * To animate to it's full height we need to know and set the full height at forehand
+	 * so we can tell the animate function to which height the span will be in it's full extended height.
+	 */
 	function setAttr_height(key) {
-		$(key).each(function(){
-			var setNormalHeight = $(this).height(); // Full height of span with text
-			$(this).attr('data-height', setNormalHeight);
-			$(this).css('height', $(this).attr('data-base') + 'px' );
-		});
+	$(key).each(function(){
+		var setNormalHeight = $(this).height(); // Full height of span with text
+		$(this).attr('data-height', setNormalHeight);
+		$(this).css('height', $(this).attr('data-base') + 'px' );
+	});
 	}
 	setAttr_height('div.read-more');
   
-  $('[data-target]').clickToggle(function(){
+	$('[data-target]').clickToggle(function(){
 		/* 
 		 * The Data-id corresponds with the data-target of the clicked button.
 		 * so you can have multiple read-more area's on one page
-     */
+		 */
 		$('[data-id="'+$(this).data('target')+'"]').animate({height: $(this).parent().prev().attr('data-height')}, 200);
 		$('[data-id="'+$(this).data('target')+'"]').find('em').fadeOut('fast');
 		$(this).text('Lees minder');
